6df7f94ed672ab34a5772c6b54cbb2299213116a
dinesh <dk23818@gmail.com>
2024-05-18 01:19:44 +0530

    Chapter 02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading", xyz: "abc" },
@@ -11,7 +14,6 @@
 // root.render(heading);
 
 // ---------------------------------------------------------
-
 
 /**
  *
@@ -31,7 +33,7 @@
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "This is Namaste React"),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
